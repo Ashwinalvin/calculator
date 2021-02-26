1862a6f9adfe3b798ece6a97f2ba92cad7c13721
Ashwinalvin <79683142+Ashwinalvin@users.noreply.github.com>
2021-02-26 18:41:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import math
+def addi(a,b):
+      return a+b
+def sub(a,b):
+    return a-b
+def multiplication(a,b):
+      return a*b
+def division(a,b):
+      if b!=0:
+          return a/b
+      if b==0:
+          print('b==0')
+def squareroot(a):
+    return math.sqrt(a)
+def squar(a):
+      return a*a
+def cubeofnum(a):
+      return a*a*a
+def exponential(a):
+      return math.exp(a)
+def log_2(a):
+      return math.log2(a)
+def pow(a,b):
+      return math.pow(a,b)
+def log_10(a):
+      return math.log10(a)
+def sino(a):
+      return math.sin(a)
+def coso(a):
+      return math.cos(a)
+def tangen(a):
+      return math.tan(a)
+
+
+a=float(input('enter a'))
+b=float(input('enter b'))
+print(addi(a,b))
+
+
+
+
+
